4deb49c1665f7d3051e7503ecf92a64e4c2435a9
Prosper Opara <prosper.opara@deimos.co.za>
2021-06-09 22:31:53 +0100

    chore: add new layout and spreadsheet support

--- src/App.js
+++ src/App.js
@@ -1,42 +1,68 @@
 import anime from 'animejs';
 import Platform from './img/platform.png';
-import logo from './img/survey_camp_logo.png'
+import logo from './img/Theodolite.png'
 import './App.css';
-import { useEffect, useRef, useState } from 'react';
-import { notifyOk, notifyError } from './utils/toaster';
-
-const encode = (data) => {
-  return Object.keys(data)
-      .map(key => encodeURIComponent(key) + "=" + encodeURIComponent(data[key]))
-      .join("&");
-}
+import { useEffect, useRef } from 'react';
+import { useFormik } from 'formik';
+import * as Yup from 'yup';
+import { notifyOk, notifyError, ngnStates, api } from './utils/toaster';
 
 function App() {
-  const [formState, setFormState] = useState({ email: '' });
-  const [error, setError] = useState('');
   let customRAF = useRef(window)
 
-  const handleSubmit = (e) => {
-    if (formState.email === '') {
-      setError('Enter a valid email address');
-      notifyError('Try again 😖')
-    } else {
-      fetch("/", {
-        method: "POST",
-        headers: { "Content-Type": "application/x-www-form-urlencoded" },
-        body: encode({ "form-name": "demoRequest", ...formState })
-      })
-      .then(() => notifyOk('Thanks for requesting a demo 🤗'))
-      .catch(error => notifyError(`${error.message} 😖`));
-      setError('')
+  const formik = useFormik({
+    initialValues: {
+      companyName: '',
+      companyEmail: '',
+      companyPhone: '',
+      companyAddress: '',
+      state: ''
+    },
+    validationSchema: Yup.object({
+      companyName: Yup.string()
+        .required('Company name is required'),
+      companyPhone: Yup.string()
+        .max(11, 'The normal phone number length is 11')
+        .required('Company phone number is required'),
+      companyEmail: Yup.string().email('Invalid email address').required('Company email is required'),
+      companyAddress: Yup.string().required('Company address is required'),
+      state: Yup.string().required('Please include the state where your company is located')
+    }),
+    onSubmit: (values, { setSubmitting, resetForm }) => {
+      const submitForm = async () => {
+        const getEmail = await api.get(`/exec?action=checkEmail&searchString=${values.companyEmail}`)
+
+        if (getEmail.data.result === 'success' && getEmail.data.message === 'email already registered') {
+          notifyError('We already have your details. We will email you when we launch');
+          setSubmitting(false)
+          resetForm();
+          return;
+        }
+
+        const formData = new FormData();
+
+        for (const key in values) {
+          formData.append(key, values[key]);
+        }
+
+        const rqt = await api.post('/exec', formData, { headers: { 'Content-Type': `multipart/form-data` } });
+
+        if (rqt.data.result === 'success') {
+          notifyOk ("Awesome, you've been added to our waiting list. You'll be the first to know once we launch")
+          setSubmitting(false);
+          resetForm();
+        }
     }
 
-    e.preventDefault();
-  };
+    setTimeout(() => {
+        submitForm().catch(error => {
+        setSubmitting(false);
+        notifyError(error.message)
+      })
+    }, 1000)
+  }})
 
-  const handleChange = (value) => {
-    setFormState({ email: value})
-  }
+
 
   useEffect(() => {
     const animation = anime({
@@ -53,7 +79,7 @@ function App() {
       customRAF.current.requestAnimationFrame(loop);
     }
     requestAnimationFrame(loop);
-  }, [error])
+  }, [])
 
   return (
       <div className="App">
@@ -63,29 +89,80 @@ function App() {
           </div>
         </header>
 
-      <article className="container-fluid">
+      <article className="container-fluid hero">
         <div className="row no-gutter justify-content-center marketing">
-          <section className="col-lg-4 col-md-5 col-xs-12 col-sm-12">
-            <h1>Introducing Surveycamp</h1>
+          <section className="col-lg-5 col-md-5 col-xs-12 col-sm-12">
+            <h1>Surveycamp 🚀</h1>
+            <h2>Go digital and do things On-Demand</h2>
             <p>
-              Surveycamp is a bespoke project management platform for managing your
-              clients expectations, site operations and office work. We aim to be the
-              one stop shop for managing your projects from start to finish.
+              Traditionally file management has been done manually in most survey firms.
+              This comes with lots of manual file achiving which is inefficient and insecure 😩 <br/><br/>
+
+              Surveycamp is a bespoke platform that lets surveyors go digital and manage client projects
+              effortlessly.
             </p>
 
-            <form onSubmit={handleSubmit}>
-              <label htmlFor="email">Enter company email to request demo</label>
-              {error !== '' ? <span>{error}</span> : null}
-              <input onChange={(e) => handleChange(e.target.value)} id="email" name="email" type="email" placeholder="enter your company email address" />
-              <button type="submit">Request for Demo</button>
-            </form>
-            <p>For other enquiries email us at <em>info.surveycamp@gmail.com</em></p>
+            <a href="#waitlist"><button type="submit">Get notified when we launch</button></a>
           </section>
 
-          <section className="col-lg-4 col-md-5 col-sm-12 col-xs-12 platform" id="platform">
+          <section className="col-lg-5 col-md-5 col-sm-12 col-xs-12 platform" id="platform">
             <img src={Platform} alt="platform" />
           </section>
         </div>
+      </article>
+
+      <article id="waitlist">
+        <section className="container">
+          <form onSubmit={formik.handleSubmit}>
+            <h2>Join Our Wait List 📆</h2>
+            <div className="inputroup">
+              <label htmlFor="companyName">Company name</label>
+              {formik.touched.companyName && formik.errors.companyName ? (<div className="error">{formik.errors.companyName}</div>) : null}
+              <input
+                id="companyName"
+                name="companyName"
+                value={formik.values.companyName}
+                onChange={formik.handleChange}
+                onBlur={formik.handleBlur}
+                type="text" 
+                placeholder="Enter company name"
+              />
+            </div>
+
+            <div className="formGroup">
+              <div className="inputGroup">
+                <label htmlFor="companyEmail">Email</label>
+                {formik.touched.companyEmail && formik.errors.companyEmail ? (<div className="error">{formik.errors.companyEmail}</div>) : null}
+                <input id="companyEmail" name="companyEmail" value={formik.values.companyEmail} onBlur={formik.handleBlur} onChange={formik.handleChange} type="email" placeholder="Enter company email"/>
+              </div>
+
+              <div className="inputGroup">
+                <label htmlFor="companyPhone">Phone number</label>
+                {formik.touched.companyPhone && formik.errors.companyPhone ? (<div className="error">{formik.errors.companyPhone}</div>) : null}
+                <input id="companyPhone" name="companyPhone" value={formik.values.companyPhone} onBlur={formik.handleBlur} onChange={formik.handleChange} type="tel" placeholder="Enter working phone number" />
+              </div>
+            </div>
+
+            <div className="formGroup">
+              <div className="inputGroup">
+                <label htmlFor="companyAddress">Office Address</label>
+                {formik.touched.companyAddress && formik.errors.companyAddress ? (<div className="error">{formik.errors.companyAddress}</div>) : null}
+                <input id="companyAddress" name="companyAddress" value={formik.values.companyAddress} onBlur={formik.handleBlur} onChange={formik.handleChange} type="text"  placeholder="Enter office address"/>
+              </div>
+
+              <div className="inputGroup">
+                <label htmlFor="state">State</label>
+                {formik.touched.state && formik.errors.state ? (<div className="error">{formik.errors.state}</div>) : null}
+                <select id="state" name="state" value={formik.values.state} onBlur={formik.handleBlur} onChange={formik.handleChange}>
+                  <option>-- select state --</option>
+                  {ngnStates.map(state => (<option value={state}>{state}</option>))}
+                </select>
+              </div>
+            </div>
+
+            <button type="submit" disabled={formik.isSubmitting}>Join waitlist</button>
+          </form>
+        </section>
       </article>
         
       <footer>
--- src/utils/toaster.js
+++ src/utils/toaster.js
@@ -1,7 +1,53 @@
 import { toast } from 'react-toastify';
+import axios from 'axios';
 import 'react-toastify/dist/ReactToastify.css';
 import './toaster.css';
 
 toast.configure(1000);
 export const notifyOk = (message) => toast.success(message);
 export const notifyError = (message) => toast.error(message);
+
+const url = 'https://script.google.com/macros/s/AKfycbzavRlnF9q2Uqdp8mUUPUG6lDn-W8Dx3Vm9-ORSEXYYdGD9ve1HqEmRMY7PJ9PeJpYL0w';
+export const api = axios.create({
+    baseURL: url,
+});
+
+export const ngnStates = [
+'Abuja',
+'Abia',
+'Adamawa',
+'Akwa Ibom',
+'Anambra',
+'Bauchi',
+'Bayelsa',
+'Benue',
+'Borno',
+'Cross River',
+'Delta',
+'Ebonyi',
+'Edo',
+'Ekiti',
+'Enugu',
+'Gombe',
+'Imo',
+'Jigawa',
+'Kaduna',
+'Kano',
+'Katsina',
+'Kebbi',
+'Kogi',
+'Kwara',
+'Lagos',
+'Nassarawa',
+'Niger',
+'Ogun',
+'Ondo',
+'Osun',
+'Oyo',
+'Plateau',
+'Rivers',
+'Sokoto',
+'Taraba',
+'Yobe',
+'Zamfara',
+]
